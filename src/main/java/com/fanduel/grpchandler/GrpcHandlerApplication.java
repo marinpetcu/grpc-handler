354911675e2b0ab43f640bd92373dd7ef1eb6f53
Marin Petcu <marin.petcu@fanduel.com>
2024-03-18 15:20:34 +0200

    setup

---
+++ src/main/java/com/fanduel/grpchandler/GrpcHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.fanduel.grpchandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcHandlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fanduel/grpchandler/GrpcHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanduel.grpchandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
